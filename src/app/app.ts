eb5873647786f873542b21de5ec005fef08b4079
VossiDev09 <tim.vos19@gmail.com>
2026-06-03 14:36:47 +0200

    fix: correct styleUrl to styleUrls in app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('one-pager');
