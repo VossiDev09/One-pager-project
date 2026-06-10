71172347e128dcbaa0b049a12cf43c7423ee412a
VossiDev09 <tim.vos19@gmail.com>
2026-06-10 12:48:23 +0200

    feat: implement responsive layout with boxes and posts sections

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,22 @@
 import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('one-pager');
+  protected readonly personalBoxes = [
+    { title: 'Box 1', text: 'Inhalt für Box 1' },
+    { title: 'Box 2', text: 'Inhalt für Box 2' },
+    { title: 'Box 3', text: 'Inhalt für Box 3' },
+  ];
+  protected readonly posts = [
+    { title: 'Box 1', text: 'Inhalt für Box 1' },
+    { title: 'Box 2', text: 'Inhalt für Box 2' },
+    { title: 'Box 3', text: 'Inhalt für Box 3' },
+  ]
 }
