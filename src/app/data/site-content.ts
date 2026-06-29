d5d18d1bb86101f454544ad7822e5e82964d62b2
Tim Vosshans <Tim.Vosshans@goldbeck.de>
2026-06-29 15:01:03 +0200

    fix: remove duplicate entries from POSTS array

--- src/app/data/site-content.ts
+++ src/app/data/site-content.ts
@@ -33,18 +33,6 @@ export const POSTS: readonly Post[] = [
   { title: 'Box 1', text: 'Inhalt für Box 1' },
   { title: 'Box 2', text: 'Inhalt für Box 2' },
   { title: 'Box 3', text: 'Inhalt für Box 3' },
-  { title: 'Box 3', text: 'Inhalt für Box 3' },
-  { title: 'Box 3', text: 'Inhalt für Box 3' },
-    { title: 'Box 1', text: 'Inhalt für Box 1' },
-  { title: 'Box 2', text: 'Inhalt für Box 2' },
-  { title: 'Box 3', text: 'Inhalt für Box 3' },
-  { title: 'Box 3', text: 'Inhalt für Box 3' },
-  { title: 'Box 3', text: 'Inhalt für Box 3' },
-    { title: 'Box 1', text: 'Inhalt für Box 1' },
-  { title: 'Box 2', text: 'Inhalt für Box 2' },
-  { title: 'Box 3', text: 'Inhalt für Box 3' },
-  { title: 'Box 3', text: 'Inhalt für Box 3' },
-  { title: 'Box 3', text: 'Inhalt für Box 3' },
 ];
 
 export const SOCIAL_LINKS: readonly SocialLink[] = [
